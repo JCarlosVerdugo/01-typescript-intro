e9e43ae5d40d404557c1416281ce7f072ba27a09
Carlos Verdugo <carlos@adroitoverseas.com>
2024-04-09 15:03:30 -0700

    05 homework

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import './style.css'
 // TOPICS
 // import './topics/01-basic-types';
 // import './topics/02-object-interface';
-import './topics/03-functions';
+// import './topics/03-functions';
+import './topics/04-homeworks-types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola
