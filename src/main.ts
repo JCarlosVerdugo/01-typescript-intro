138a222c87343f92b07b99d8c212bc2b4e42f41f
Carlos Verdugo <carlos@adroitoverseas.com>
2024-04-09 14:53:09 -0700

    03 functions

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import './style.css'
 
 // TOPICS
 // import './topics/01-basic-types';
-import './topics/02-object-interface';
+// import './topics/02-object-interface';
+import './topics/03-functions';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola
