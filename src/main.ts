37cdd6518f1a2d92edb61537b19a41ff760d02d2
Carlos Verdugo <carlos@adroitoverseas.com>
2024-04-09 14:19:55 -0700

    Basics tpyes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
+import './topics/01-basic-types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Carlos';
+let hpPoints: number | string = 95;
+const isAlive: boolean = true;
+
+hpPoints = 60;
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+export {};
