5c8e9053ed77aa58c2293358781ade6a823d8d5d
Carlos Verdugo <carlos@adroitoverseas.com>
2024-04-11 10:09:36 -0700

    08 composition classes

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -19,21 +19,35 @@ export class Person {
 }
 
 
-export class Hero extends Person {
+// export class Hero extends Person {
+
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ) {
+//         super( realName, 'New York' );
+//     }
+
+// }
+
+
+export class Hero {
 
     constructor(
         public alterEgo: string,
         public age: number,
         public realName: string,
+        public person: Person,
     ) {
-        super( realName, 'New York' );
+        this.person = new Person(realName)
     }
 
 }
 
 
-
-const ironman = new Hero('Ironman', 45, 'Tony Stark');
+const tony = new Person('Tony Stark', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony Stark', tony);
 
 // console.log(ironman.name);
 // // private properties can access in typescript
