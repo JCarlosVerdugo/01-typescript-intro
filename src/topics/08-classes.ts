a69df02e074afdde77077ed612d9395b5145f9a2
Carlos Verdugo <carlos@adroitoverseas.com>
2024-04-11 10:00:50 -0700

    08 extend classes

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -19,7 +19,21 @@ export class Person {
 }
 
 
-const ironman = new Person('Ironman');
+export class Hero extends Person {
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ) {
+        super( realName, 'New York' );
+    }
+
+}
+
+
+
+const ironman = new Hero('Ironman', 45, 'Tony Stark');
 
 // console.log(ironman.name);
 // // private properties can access in typescript
